0035e15f6a10a78f9f896a20ad5d30e276ee053f
PepsKi <104223970+pepski1270@users.noreply.github.com>
2022-08-08 22:11:04 +0800

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,6 +1,6 @@
 Config = {
     webhookColor = 16711680, --EMBED COLOR
-    name = "Community Name Here" -- NAME
+    name = "", -- NAME
     logo         = "https://via.placeholder.com/30x30", --HEAD LOGO
     footerLogo   = "https://via.placeholder.com/30x30", --FOOTER LOGO
     Avatar       = "https://via.placeholder.com/30x30", -- AVATAR LOGO
@@ -9,10 +9,8 @@ Config = {
     steamURL = true,
     discordID = true,
     license = true,
-    license2 = true,
     IP = true,
 
-    combatlog = true, -- change to false if u dont want combat logging to be recorded
     deathlog = "",
     joining = "",
     leaving = "",
